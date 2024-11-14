2b6e91447bbff3c6a24f68121b71e40a0d4d0af6
yagovysk <yago09regis@gmail.com>
2024-11-14 14:42:50 -0300

    adicionando elementos e background

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { About } from "./Components/About/About";
 import { Menu } from "./Components/Menu/Menu";
+import { Start } from "./Components/Start/Start";
 
 export function App() {
   return (
     <main>
       <Menu />
+      <Start />
+      <About />
     </main>
   );
 }
--- src/Components/Start/Start.jsx
+++ src/Components/Start/Start.jsx
@@ -1,5 +1,20 @@
 import "./Start.css";
 
 export function Start() {
-  return <section></section>;
+  return (
+    <section className="start-section">
+      <article className="article-start">
+        <h1>Título inicial</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Exercitationem est excepturi quos maiores provident quam, nulla animi
+          libero et aspernatur, deleniti iusto id vero commodi, cum dolorum nam
+          quisquam. Beatae.
+        </p>
+      </article>
+      <a href="" className="prospection-button">
+        Botão para contato
+      </a>
+    </section>
+  );
 }
