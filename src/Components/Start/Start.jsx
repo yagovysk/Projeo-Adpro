de9aa0dad201cb816eb31239d4401d1fb017c0dd
yagovysk <yago09regis@gmail.com>
2024-11-14 17:12:44 -0300

    adicionei um video ao fundo apra ficar melhor

--- src/Components/Start/Start.jsx
+++ src/Components/Start/Start.jsx
@@ -1,20 +1,42 @@
+import backgroundVideo from "../../assets/background-start.mp4";
+import { useEffect, useRef } from "react";
 import "./Start.css";
 
 export function Start() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.8;
+    }
+  }, []);
+
   return (
     <section className="start-section">
-      <article className="article-start">
-        <h1>Título inicial</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Exercitationem est excepturi quos maiores provident quam, nulla animi
-          libero et aspernatur, deleniti iusto id vero commodi, cum dolorum nam
-          quisquam. Beatae.
-        </p>
-      </article>
-      <a href="" className="prospection-button">
-        Botão para contato
-      </a>
+      <video
+        ref={videoRef}
+        className="video-background"
+        src={backgroundVideo}
+        autoPlay
+        loop
+        muted
+        playsInline
+      ></video>
+
+      <div className="content-overlay">
+        <article className="article-start">
+          <h1>Título inicial</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Exercitationem est excepturi quos maiores provident quam, nulla
+            animi libero et aspernatur, deleniti iusto id vero commodi, cum
+            dolorum nam quisquam. Beatae.
+          </p>
+        </article>
+        <a href="#" className="prospection-button">
+          Botão para contato
+        </a>
+      </div>
     </section>
   );
 }
